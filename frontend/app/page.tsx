a2f5aab32950264d92c274cc986dc068fbd56438
Muhammad <samahkinets@gmail.com>
2026-05-03 04:01:48 +0300

    Fixed iframe

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -291,6 +291,8 @@ export default function HomeGamesPage() {
           <iframe
             src={`${activeGame}?clid=${process.env.NEXT_PUBLIC_CLID}`}
             className="flex-1 w-full border-none"
+            // sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+            // sandbox="allow-same-origin allow-scripts"
             allow="fullscreen; accelerometer; camera; clipboard-read; clipboard-write; screen-wake-lock; speaker-selection; web-share; geolocation; gyroscope; microphone; xr-spatial-tracking; autoplay; encrypted-media; picture-in-picture; payment; publickey-credentials-get; publickey-credentials-create; storage-access; attribution-reporting; browsing-topics"
           />
         </div>
